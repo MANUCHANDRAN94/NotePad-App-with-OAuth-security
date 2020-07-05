93d966ac82d709e96a8a54dcaac84535c3c62328
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-07-05 10:24:26 +0530

    Authentication success

--- app.js
+++ app.js
@@ -21,22 +21,22 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(session({
-  secret:"mittu poocha",
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: "mittu poocha",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 mongoose.connect("mongodb://localhost:27017/todoDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-mongoose.set("useCreateIndex", true);    //to remove this (DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.)
-
+mongoose.set("useCreateIndex", true); //to remove this (DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.)
 
 const itemsSchema = {
   itemName: String,
@@ -45,16 +45,14 @@ const Item = mongoose.model("Item", itemsSchema);
 
 const listSchema = {
   name: String,
-  items: [itemsSchema]
+  items: [itemsSchema],
 };
 const List = mongoose.model("List", listSchema);
 
-
-
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  content: listSchema
+  content: listSchema,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -63,21 +61,15 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
     done(err, user);
   });
 });
-
-
-
-
-
-
 
 app.get("/signup", function (req, res) {
   res.render("signup");
@@ -90,23 +82,17 @@ app.get("/logout", function (req, res) {
   res.redirect("/");
 });
 
-
-
-
 app.get("/listmenu", function (req, res) {
-  console.log(req.body);
-  if(req.isAuthenticated()){
-    console.log('REACHED GETLISTMENU IF CASE');
-    
+  if (req.isAuthenticated()) {
     res.render("listmenu", { todaysDate: date() });
   } else {
-    res.redirect("/")
+    res.redirect("/");
   }
 });
 
-app.get("/:customListName", function (req, res) {
+app.get("/listmenu/:customListName", function (req, res) {
   const customListName = _.capitalize(req.params.customListName);
-
+//give authentication and req.user._id
   List.findOne({ name: customListName }, function (err, foundList) {
     if (!err) {
       if (!foundList) {
@@ -115,12 +101,11 @@ app.get("/:customListName", function (req, res) {
           name: customListName,
           items: [],
         });
-        list.save(function(err,data){
-          if(!err){
+        list.save(function (err, data) {
+          if (!err) {
             res.redirect("/" + customListName);
           }
         });
-       
       } else {
         //Show an existing list
 
@@ -134,81 +119,70 @@ app.get("/:customListName", function (req, res) {
   });
 });
 
+app.post("/add", function (req, res) {
+  const itemm = req.body.newItem;
+  const listName = req.body.list;
 
-app.post("/add", function(req, res){
-
-    const itemm = req.body.newItem;
-    const listName = req.body.list;
-  
-    const item = new Item({
-        itemName: itemm
-    });
-  
-      List.findOne({name: listName}, function(err, foundList){
-        foundList.items.push(item);
-        foundList.save();
-        res.redirect("/" + listName);
-      });
-    
+  const item = new Item({
+    itemName: itemm,
   });
 
-  app.post("/delete", function(req, res){
-    const checkedItemId = req.body.checkbox;
-    const listName = req.body.listName;
-
-      List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
-        if (!err){
-          res.redirect("/" + listName);
-        }
-      });
- 
-  
+  List.findOne({ name: listName }, function (err, foundList) {
+    foundList.items.push(item);
+    foundList.save();
+    res.redirect("/listmenu/" + listName);
   });
+});
 
-  app.post("/signup", function(req,res){
-    console.log('REACHED IN SIGNUP');
-    
-    User.register({username: req.body.email}, req.body.pass, function(err, user){
-      if (err) {
-        console.log(err);
-        res.redirect("/signup");
-      } else {
-        passport.authenticate("local")(req, res, function(){
-          res.redirect("/listmenu");
-        });
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  List.findOneAndUpdate(
+    { name: listName },
+    { $pull: { items: { _id: checkedItemId } } },
+    function (err, foundList) {
+      if (!err) {
+        res.redirect("/listmenu/" + listName);
       }
-    });
+    }
+  );
+});
+
+app.post("/signup", function (req, res) {
+  console.log("REACHED IN SIGNUP");
+
+  User.register({ username: req.body.username }, req.body.password, function (
+    err,
+    user
+  ) {
+    if (err) {
+      console.log(err);
+      res.redirect("/signup");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/listmenu");
+      });
+    }
+  });
+});
+
+app.post("/", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
   });
 
-
-  app.post("/", function(req, res){
-
-    
-
-    const user = new User({
-      username: req.body.your_name,
-      password: req.body.your_pass
-    });
-
-    console.log(user);
-  
-    req.login(user, function(err){  //cjheck what this function does.
-      if (err) {
-        console.log(err);
-      } else {
-        passport.authenticate("local",
-        //  { successRedirect: '/listmenu',
-        // failureRedirect: '/' });
-       // (req, res, 
-          function(){
-          console.log('REACHED IN SIGnin else');
-          res.redirect("/listmenu");
-        });
-      }
-    });
-  
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/listmenu");
+      });
+    }
   });
-
+});
 
 app.listen(port, function (req, res) {
   console.log(`server started running at http://${host}:${port}`);

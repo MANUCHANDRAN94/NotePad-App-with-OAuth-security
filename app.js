a636040334fd923d8c0b05ce7ebd4a6fbecff24a
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-07-08 17:59:35 +0530

    testing phase 2

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-mongoose.connect("mongodb://localhost:27017/todoDB", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useFindAndModify: false,
-});
+},(err)=>{if(err){console.log(err)}else{console.log("Database succesfully connected!")}});
 mongoose.set("useCreateIndex", true); //to remove this (DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.)
 
 const itemsSchema = {
